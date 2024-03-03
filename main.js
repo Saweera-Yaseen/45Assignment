fa0fd2c8b8186fe4d4d754bc209e98de71ed7920
Saweera-Yaseen <160421869+Saweera-Yaseen@users.noreply.github.com>
2024-03-03 20:19:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//Question#!: Install Node.js, TypeScript and VS Code on your computer
+//Question#2:Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var Person = "Saweera";
+console.log(Hello, $, { Person: Person }, would, you, like, to, learn, python, today ?  : );
