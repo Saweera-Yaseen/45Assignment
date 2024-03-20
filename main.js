4e39e48139e29a06c2809ed68ab4ad8e108a1902
Saweera Yaseen <160421869+Saweera-Yaseen@users.noreply.github.com>
2024-03-20 19:28:39 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
-//Question#4 Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
-var myFvrtQuote = "A person who never made a mistake never tried anything new.";
-var author = "Albert Einstein";
-console.log(myFvrtQuote, author);
-////Question#5 Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
-var famous_person = "Steve Jobs";
+//Question#6 Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+// for \t
+var myname = "\tSufyan\t";
+console.log(myname);
+//for \n
+var othername = "\nSufyan Yaseen\n";
+console.log(othername);
+//Question#7 Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//Addition
+var nmbr1 = 2;
+var nmbr2 = 6;
+var sum = nmbr1 + nmbr2;
+console.log(sum);
+//Subtraction
+var nmbr3 = 12;
+var nmbr4 = 4;
+var subtract = nmbr3 - nmbr4;
+console.log(subtract);
+//Multiplication
+var nmbr5 = 4;
+var nmbr6 = 2;
+var multiply = nmbr5 * nmbr6;
+console.log(multiply);
+//Division
+var nmbr7 = 80;
+var nmbr8 = 10;
+var divide = nmbr7 / nmbr8;
+console.log(divide);
+//Question#8 You should create four lines that look like this:
+//console.log(5 + 3)
+//Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(5 + 3);
+console.log(12 - 4);
+console.log(4 * 2);
+console.log(80 / 10);
+//Question# 9 Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var fvrtNmbr = 5;
+var message = "This is my fvrt nmbr";
+console.log(message, fvrtNmbr);
--- main.ts
+++ main.ts
@@ -1,10 +1,31 @@
-//Question#4 Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//Question#3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let FirstName:string="Saweera"
+console.log(FirstName.toLowerCase());
 
-let myFvrtQuote:string="A person who never made a mistake never tried anything new.";
-let author="Albert Einstein";
+// in Upercase
+console.log(FirstName.toUpperCase());
 
-console.log(myFvrtQuote,author);
+// in titlecase
+console.log(FirstName.replace(/\b\w/g,c=> c.toUpperCase()));
 
-////Question#5 Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+//Question#5 Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
 
-let famous_person="Steve Jobs"
+
+let famous_person="Steve Jobs";
+let message="The only way to do great workis to love what you do.";
+
+console.log(famous_person, message);
+
+//Question#6 Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+//for\t
+let personName:string="\tSaweera\t";
+console.log(personName);
+
+//for\n
+let myname:string="\nSaweera Yaseen\n";
+console.log(myname);
+
+//for combination  
+let othername: string="\nSaweera Arain\t";
+console.log(othername);
