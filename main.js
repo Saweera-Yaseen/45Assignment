6ab221f261dec0c078f598f14caacf0f46daa6ab
Saweera Yaseen <160421869+Saweera-Yaseen@users.noreply.github.com>
2024-03-21 20:17:51 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
-//Question#9Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-var favouriteNmbr = "5";
-var message = "This is my favouriteNmbr";
-console.log(message, favouriteNmbr);
-//Question#10 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
-// Program by Saweera
-// Date: March 14, 2024
-var myFavoriteQuote = "A person who never made a mistake never tried anything new.";
-var author = "Albert Einstein";
-console.log(myFavoriteQuote, author);
-// Program by Saweera
-// Date: March 14, 2024
-//for\t
-var personName = "\tSaweera\t";
-console.log(personName);
-//for\n
-var myname = "\nSaweera Yaseen\n";
-console.log(myname);
-//for combination  
-var othername = "\nSaweera Arain\t";
-console.log(othername); //for\t
-var personName = "\tSaweera\t";
-console.log(personName);
-//for\n
-var myname = "\nSaweera Yaseen\n";
-console.log(myname);
-//for combination  
-var othername = "\nSaweera Arain\t";
-console.log(othername);
-//Question#11 Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-var names = "Faiza, Rafia, Hina";
-for (var i = 0; i < names.length; i++) {
-    console.log(names[i]);
+//Question#12 Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+var fvrtfrnd = ["Faiza, Rafia, Hina"];
+for (var _i = 0, fvrtfrnd_1 = fvrtfrnd; _i < fvrtfrnd_1.length; _i++) {
+    var frnds = fvrtfrnd_1[_i];
+    console.log("Dear", frnds, "Thank you for joining us today");
 }
 ;
+//Question#13 Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+var transportation = ["Tesla Model S", "Honda CBR1000RR"];
+for (var _a = 0, transportation_1 = transportation; _a < transportation_1.length; _a++) {
+    var message = transportation_1[_a];
+    console.log("vehicle", message, "I would like to own a Honda motorcycle.");
+}
+;
+//Question#14 Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+var guests = ["Ashfaq, Erum,Faiza"];
+for (var _b = 0, guests_1 = guests; _b < guests_1.length; _b++) {
+    var guest = guests_1[_b];
+    ;
+}
+{
+    console.log("Dear", guests, "I would like to invite you at dinner");
+}
+//Question#16 Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+var unavailableguest = "Ashfaq";
+var msgF = "Unfortunatily {Ashfaq}!, can't makr it!";
+console.log(msgF, (unavailableguest = unavailableguest));
--- main.ts
+++ main.ts
@@ -1,30 +1,18 @@
+//Question#12 Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
 
-//Question#10 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+let fvrtfrnd=["Faiza, Rafia, Hina"];
+for (let frnds of fvrtfrnd)
+{console.log("Dear",frnds,"Thank you for joining us today")};
 
-// Program by Saweera
-// Date: March 14, 2024
-let myFavoriteQuote = "A person who never made a mistake never tried anything new.";
-let author = "Albert Einstein";
+//Question#13 Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 
-console.log(myFavoriteQuote, author);
+let transportation= ["Tesla Model S", "Honda CBR1000RR"]
+for (let message of transportation)
+{console.log("vehicle",message, "I would like to own a Honda motorcycle.")};
 
-// Program by Saweera
-// Date: March 14, 2024
-//for\t
-let personName:string="\tSaweera\t";
-console.log(personName);
+//Question#14 Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
 
-//for\n
-let myname:string="\nSaweera Yaseen\n";
-console.log(myname);
+let guests=["Ashfaq, Erum,Faiza"];
+for (let guest of guests);
+{console.log("Dear" ,guests,"I would like to invite you at dinner")}
 
-//for combination  
-let othername: string="\nSaweera Arain\t";
-console.log(othername);
-
-
-//Question#11 Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-
-let names:string="Faiza, Rafia, Hina";
-for (let i = 0; i < names.length; i++) {
-            console.log(names[i])};
