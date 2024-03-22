c64b41aae1784519bee459e144550f4c090b5aca
Saweera Yaseen <160421869+Saweera-Yaseen@users.noreply.github.com>
2024-03-22 07:55:11 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,6 @@
-//Question#15 Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
-//Question 16 More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite them on dinner.
-// Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
-// Add one new guest to the beginning of your array.
-//Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.
-//Initial guest list
-var guest_list = ["Sufyan"];
-// Function to print invitation messages
-var guests = ["Sufyan"];
-for (var guest in guests) {
-    console.log("Dear {guest},\nYou are cordially invited to dinner at my place on Saturday night. Please RSVP.");
-}
-// Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
-var dontCome = guests[0];
-console.log(dontCome, "can not make it");
-// Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
-guest_list.splice(0, 1, "Sufyan Yaseen");
-// Print a second set of invitation messages, one for each person who is still in your list.
+//Queston#17 
 var gusts = ["Esha, Ayesha,Fatima"];
-for (var _i = 0, gusts_1 = gusts; _i < gusts_1.length; _i++) {
-    var guests_1 = gusts_1[_i];
-    console.log("Dear", guests_1, "You are invited to dinner");
-}
-console.log("Dear", guests, "I have found a bigger dinner table");
+console.log("Dear", gusts, "I have found a bigger dinner table");
 //Adding new value at beginning
 gusts.unshift("Shahnawaz");
 console.log(gusts);
@@ -37,3 +16,16 @@ gusts.splice(middleIndex, 0, "Aqsa");
 console.log("Updated list of our Gusts");
 //seeking invitation one by one
 gusts.forEach(function (oneguest) { return console.log("Hello ".concat(oneguest, ", would you like to come dinner with me")); });
+//Inform that only 2 guests is invited
+console.log("Unfortunatily, the new dinner table wont arrive on time, so I can only inviteteo guests dinner with me");
+//Using while_loop to remove guests from array
+while (gusts.length > 2) {
+    var removegusts = gusts.pop();
+    console.log("Sorry, ".concat(removegusts, " I can not invite you at dinner"));
+}
+// Printing invitations to last two gustes
+console.log("Invitaton to the the last teo gusts");
+gusts.forEach(function (lasttwo) { return console.log("Luckely ".concat(lasttwo, ", you are invited to dinner")); });
+gusts.pop();
+gusts.pop();
+console.log("Empty list", gusts);
--- main.ts
+++ main.ts
@@ -1,40 +1,12 @@
-//Question#15 Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+//Queston#17 Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
 
-//Question 16 More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite them on dinner.
-
-// Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
-
-// Add one new guest to the beginning of your array.
-
-//Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.
-
-
-//Initial guest list
-let guest_list = ["Sufyan"]
-
-// Function to print invitation messages
-let guests=["Sufyan"]
-    for (let guest in guests)
-      {console.log("Dear {guest},\nYou are cordially invited to dinner at my place on Saturday night. Please RSVP.")}
-
-// Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
-
-let dontCome= guests[0];
-console.log(dontCome, "can not make it");
-
-// Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
-
-guest_list.splice(0, 1, "Sufyan Yaseen");
-
-
-// Print a second set of invitation messages, one for each person who is still in your list.
+// Start with your program from Exercise 16. Add a new line that prints a message saying that you can invite only two people for dinner.
 
 let gusts=["Esha, Ayesha,Fatima"]
 
-for (let guests of gusts)
-{console.log("Dear" ,guests,"You are invited to dinner")}
+//Print a message to each of the two people still on your list, letting them know they’re still invited.
 
-console.log("Dear",guests, "I have found a bigger dinner table")
+console.log("Dear",gusts, "I have found a bigger dinner table")
 
 //Adding new value at beginning
 gusts.unshift("Shahnawaz")
@@ -55,4 +27,23 @@ gusts.splice(middleIndex, 0, "Aqsa")
 console.log("Updated list of our Gusts");
 
 //seeking invitation one by one
-gusts.forEach(oneguest => console.log(`Hello ${oneguest}, would you like to come dinner with me`))
+gusts.forEach(oneguest => console.log(`Hello ${oneguest}, would you like to come dinner with me`));
+
+//Inform that only 2 guests is invited
+console.log("Unfortunatily, the new dinner table wont arrive on time, so I can only inviteteo guests dinner with me");
+
+//Remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+while(gusts.length > 2){
+    let removegusts=gusts.pop()
+console.log(`Sorry, ${removegusts} I can not invite you at dinner`);
+}
+// Printing invitations to last two gustes
+console.log("Invitaton to the the last teo gusts");
+gusts.forEach(lasttwo => console.log(`Luckely ${lasttwo}, you are invited to dinner`));
+
+//Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+
+gusts.pop();
+gusts.pop();
+
+console.log("Empty list", gusts);
