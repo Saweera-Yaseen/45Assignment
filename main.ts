a3697c057d1ca3a94c4c8cbc2c82f05208d11aac
Saweera Yaseen <160421869+Saweera-Yaseen@users.noreply.github.com>
2024-04-30 20:27:49 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,18 +1,37 @@
-// Question 37 Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+// Question 38 Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value.
 
-// Making a function
+//Call your function for three different cities, at least one of which is not in the default country.
 
-function make_shirt (size: string = "Large", printMessage: string = "I Love Typescript"){
-    
-    console.log(`Creationg ${size} shirt with the ${printMessage} prints on shirt.`)
+function describe_city (city: string, country: string = "Pakistan"){
+
+    console.log(`${city} is in ${country}`);
 
 }
 
-// Calling a function by defult
-make_shirt();
+//calling the function
+describe_city ("Karachi");
 
-// Calling a function now with Medium size defult message
-make_shirt("Medium")
+describe_city ("Lahore")
 
-// Calling a function now with small size and also different print message
-make_shirt("Small", "I Love JavaScript")
+describe_city ("Berlin", "Germany");
+
+// Question # 39 City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+
+//"Lahore, Pakistan"
+
+//Call your function with at least three city-country pairs, and print the value that’s returned.
+
+function city_country(city: string, country: string) : string{
+
+    return `${city} , ${country}`;
+
+} 
+
+// calling a function and print a written value 
+;
+
+console.log(city_country("Karachi", "Pakistan"));
+
+console.log(city_country("Tokyo", "Japan"));
+
+console.log(city_country("Berlin", "Germany"));
